93f3e7c188dae79cba56b55fdd93d3553716c34f
Gustavo de Oliveira <gustavooliveirabrunosilva@gmail.com>
2021-01-03 16:52:32 -0300

    First commit

---
+++ src/main/java/com/example/SgBuildingARestfulWebserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgBuildingARestfulWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgBuildingARestfulWebserviceApplication.class, args);
+	}
+
+}
